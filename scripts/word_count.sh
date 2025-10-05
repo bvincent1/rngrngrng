111ab125b5c94a6eaa78ba77ef5cf410fc9874b7
Ben Vincent <benjc.vincent@gmail.com>
2025-10-05 09:17:39 -0600

    Add metadata for tracking

---
+++ scripts/word_count.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+if [[ "${TRACE-0}" == "1" ]]; then
+    set -o xtrace
+fi
+
+if [[ "${1-}" =~ ^-*h(elp)?$ ]]; then
+    echo 'Usage: ./word_count.sh [file]
+
+Get word count of given file.
+
+'
+    exit
+fi
+
+# cd "$(dirname "$0")"
+
+
+main() {
+    sed '1{/^---$/! q;};1,/^---$/d' $1 | wc -w  | grep -o "[0-9]*"
+}
+
+for arg in $@
+do
+  main "$arg"
+done
